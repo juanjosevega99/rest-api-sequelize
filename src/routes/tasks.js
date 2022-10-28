648c1ca5f09f769aca66c1104c23755065e0d5dc
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-27 22:19:33 -0500

    chore: add routes task

---
+++ src/routes/tasks.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+
+const router = Router();
+const {
+  getTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask
+} = require('../controllers/tasks');
+
+router.get('/tasks', getTasks);
+router.get('/tasks/:id', getTask);
+router.post('/tasks', createTask);
+router.put('/tasks/:id', updateTask);
+router.delete('/tasks/:id', deleteTask);
+
+module.exports = router;
