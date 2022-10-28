31af3427cbc0da6e7c109c02e26e064b591d2a37
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-27 22:07:21 -0500

    feat: crud

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -9,6 +9,22 @@ const getProjects = async (req, res) => {
   }
 };
 
+const getProject = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const project = await Project.findOne({ where: { id } });
+
+    if (!project) {
+      return res.status(404).json({ message: 'Project does not exist' });
+    }
+
+    res.json(project);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 const createProjects = async (req, res) => {
   const { name, priority, description } = req.body;
 
@@ -25,7 +41,41 @@ const createProjects = async (req, res) => {
   }
 };
 
+const updateProject = async (req, res) => {
+  const { id } = req.params;
+  const { name, priority, description } = req.body;
+
+  try {
+    const project = await Project.findByPk(id);
+    project.name = name;
+    project.priority = priority;
+    project.description = description;
+    await project.save();
+
+    res.json(project);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteProject = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Project.destroy({
+      where: { id }
+    });
+
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getProjects,
-  createProjects
+  getProject,
+  createProjects,
+  updateProject,
+  deleteProject
 };
